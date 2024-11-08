a5269addda4ff5e592aba2ccda065c85dd79c1c6
Nihal Shukla <nihalshukla67952@gmail.com>
2024-11-08 15:40:41 +0530

    first commit

---
+++ src/component/Navbar.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+import './Navbar.css'
+function Navbar() {
+  return (
+    <>
+      <div className='navbar'>
+
+    <p>Home</p>
+    <p>Login</p>
+    <p>SignUp</p>
+    <p>Contact</p>
+    <p>Thankyou</p>    
+      </div>
+    </>
+  )
+}
+
+export default Navbar
